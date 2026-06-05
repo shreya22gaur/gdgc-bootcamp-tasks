02b6bfe747443f2cbae4d1ea77670f4b933b55dc
shreya22gaur <156595652+shreya22gaur@users.noreply.github.com>
2026-06-05 22:33:09 +0530

    Delete task 2 directory

--- task 2/problem2.py
+++
@@ -1,2 +0,0 @@
-def transpose_matrix(a: list[list[int|float]]) -> list[list[int|float]]:
-    return [list(i) for i in zip(*a)]
--- task 2/problem5.py
+++
@@ -1,2 +0,0 @@
-def scalar_multiply(matrix: list[list[int|float]], scalar: int|float) -> list[list[int|float]]:
-    return [[element * scalar for element in row] for row in matrix]
